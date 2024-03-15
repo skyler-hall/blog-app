22a630a2c8214386752440cce6b57d923f2adc9b
Skyler Hall <78979287+Ghostellor@users.noreply.github.com>
2024-03-15 12:06:04 -0400

    March 15th Update

---
+++ src/app/page.jsx
@@ -0,0 +1,17 @@
+import Featured from "@/components/featured/Featured";
+import CategoryList from "@/components/categoryList/CategoryList";
+import CardList from "@/components/cardList/CardList";
+import styles from "./homepage.module.css";
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+    <Featured />
+    <CategoryList />
+    <div className={styles.content}>
+      <CardList />
+    </div>
+  </div>
+);
+}
