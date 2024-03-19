fc83e3e187e81a4075077ed8b030d24dbf420618
Skyler Hall <78979287+Ghostellor@users.noreply.github.com>
2024-03-19 16:37:36 -0400

    Card Component Design

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -7,10 +7,10 @@ const Featured = () => {
     <div className={styles.container}>
       <h1 className={styles.container}><b>Hey, Culture Dev!</b>Discover my stories and creative ideas.</h1>
       <div className={styles.post}>
-        <div className={styles.imgContainer}>
+        <div className={styles.imageContainer}>
           <Image src="/p1.jpeg" alt="" fill className={styles.image}></Image>
         </div>
-        <div className={stylestextContainer}>
+        <div className={styles.textContainer}>
         <h1 className={styles.postTitle}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae quos in consequuntur et illo laboriosam quaerat laborum? Impedit cum, nesciunt qui a eligendi sed quod, nobis aliquam, exercitationem porro architecto?</h1>
           <p className={styles.postDesc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae eius mollitia suscipit quod nam repellendus eaque doloribus assumenda soluta, consequatur corrupti a aliquam eveniet vitae dicta ad, alias voluptatibus quidem.</p>
           <button className={styles.button}>Read More</button>
